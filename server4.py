a5156d9a507738f3321b1da5649e9e34754812d7
RubenM19 <92644593+RubenM19@users.noreply.github.com>
2024-11-28 19:43:35 -0500

    Update server4.py

--- server4.py
+++ server4.py
@@ -102,4 +102,4 @@ async def predict_values(values: PredictValues):
         raise HTTPException(status_code=500, detail=f"Error procesando los valores: {e}")
 
 if __name__ == "__main__":
-    uvicorn.run(app, port=8080, host='0.0.0.0')
+    uvicorn.run(app, port=8000, host='0.0.0.0')
